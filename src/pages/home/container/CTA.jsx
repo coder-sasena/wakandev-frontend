5e99fe8e4258b35a44b49622702d9b1ff1d5ceff
git-coder-id <asigitj1@gmail.com>
2023-12-27 21:37:09 +0700

    LandingPage

--- src/pages/home/container/CTA.jsx
+++ src/pages/home/container/CTA.jsx
@@ -22,33 +22,33 @@ const CTA = () => {
       </svg>
 
       <section className="relative bg-dark-hard px-5">
-        <div className="container grid grid-cols-12 mx-auto py-10">
-          <div className="col-span-12">
-            <h2 className="text-white font-roboto font-bold text-2xl">
+        <div className="container grid grid-cols-12 mx-auto py-10 md:pb-20 lg:place-items-center">
+          <div className="col-span-12 lg:col-span-6">
+            <h2 className="text-white font-roboto font-bold text-2xl md:text-4xl md:text-center md:leading-normal lg:text-left">
               Get our stories delivered From us to your inbox weekly.
             </h2>
-            <div className="w-full max-w-[494px] mt-12 space-y-3 mx-auto">
+            <div className="w-full max-w-[494px] mt-12 space-y-3 mx-auto md:space-y-0 md:flex md:items-center md:space-x-2 lg:mx-0">
               <input
                 type="text"
-                name=""
-                id=""
                 className="px-4 py-3 rounded-lg w-full placeholder:text-dark-light"
                 placeholder="Your Email"
               />
-              <button className="px-4 py-3 rounded-lg w-full bg-primary text-white font-bold">
+              <button className="px-4 py-3 rounded-lg w-full bg-primary text-white font-bold md:w-fit md:whitespace-nowrap">
                 Get started
               </button>
             </div>
-            <p className="text-dark-light text-sm leading-7 mt-6">
-              <span className="font-bold italic text-[#B3BAC5]">
+            <p className="text-dark-light text-sm leading-7 mt-6 md:text-center md:text-base lg:text-left">
+              <span className="font-bold italic text-[#B3BAC5] md:not-italic md:font-normal md:text-dark-light">
                 Get a response tomorrow
               </span>{" "}
               if you submit by 9pm today. If we received after 9pm will get a
               reponse the following day.
             </p>
           </div>
-          <div className="col-span-12 hidden mb-[70px] md:block md:order-first">
+          <div className="col-span-12 hidden mb-[70px] md:block md:order-first lg:col-span-6 lg:order-last">
             <div className="w-3/4 mx-auto relative">
+              <div className="w-1/2 h-1/2 bg-[#fc5a5a] rounded-lg absolute top-[10%] -right-[8%]" />
+              <div className="w-1/2 h-1/2 bg-white rounded-lg opacity-[.06] absolute -bottom-[10%] -left-[8%]" />
               <div className="w-full rounded-xl bg-white p-3 z-[1] relative">
                 <img
                   src={images.CtaImage}
