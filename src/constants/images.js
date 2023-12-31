60f2ba4dbf133c8e1f7def1cc9a312c8dbe9c92e
git-coder-id <asigitj1@gmail.com>
2023-12-31 08:56:55 +0700

    posts

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,13 +1,13 @@
 import Logo from "../assets/Logo.svg";
 import HeroImage from "../assets/HeroImage.svg";
-import PostImage from "../assets/posts/post1.jpg";
+import Post1Image from "../assets/posts/post1.jpg";
 import PostProfileImage from "../assets/posts/post-profile.svg";
 import CtaImage from "../assets/CtaImage.jpg";
 
 const images = {
   Logo,
   HeroImage,
-  PostImage,
+  Post1Image,
   PostProfileImage,
   CtaImage,
 };
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import MainLayout from "../../components/MainLayout";
 import Hero from "./container/Hero";
 import Articles from "./container/Articles";
