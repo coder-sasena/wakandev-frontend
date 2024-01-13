56fca8e957579cf84f89dc35234d12208fe682da
git-coder-id <asigitj1@gmail.com>
2024-01-13 08:42:27 +0700

    komentar

--- src/components/comments/Comment.jsx
+++ src/components/comments/Comment.jsx
@@ -2,10 +2,33 @@ import React from "react";
 import { FiMessageSquare, FiEdit2, FiTrash } from "react-icons/fi";
 
 import { images } from "../../constants";
+import CommentForm from "./CommentForm";
 
-const Comment = ({ comment, logginedUserId }) => {
+const Comment = ({
+  comment,
+  logginedUserId,
+  affectedComment,
+  setAffectedComment,
+  addComment,
+  parentId = null,
+  updateComment,
+  deleteComment,
+  replies,
+}) => {
   const isUserLoggined = Boolean(logginedUserId);
   const commentBelongsToUser = logginedUserId === comment.user._id;
+  const isReplying =
+    affectedComment &&
+    affectedComment.type === "replying" &&
+    affectedComment._id === comment._id;
+  const repliedCommentId = parentId ? parentId : comment._id;
+  const replyOnUserId = comment.user._id;
+  const isEditing =
+    affectedComment &&
+    affectedComment.type === "editing" &&
+    affectedComment._id === comment._id;
+  const editedCommentId = parentId ? parentId : comment._id;
+  const editOnUserId = comment.user._id;
 
   return (
     <div className="flex flex-nowrap items-start gap-x-3 bg-[#f2f4f5] p-3 rounded-lg">
@@ -26,29 +49,79 @@ const Comment = ({ comment, logginedUserId }) => {
             hour: "2-digit",
           })}
         </span>
-        <p className="font-opensans mt-[10px] text-dark-light">
-          {comment.desc}
-        </p>
+        {!isEditing && (
+          <p className="font-opensans mt-[10px] text-dark-light">
+            {comment.desc}
+          </p>
+        )}
+        {isEditing && (
+          <CommentForm
+            btnLabel="Update"
+            formSubmitHandler={(value) => updateComment(value, comment._id)}
+            formCancelHandler={() => setAffectedComment(null)}
+            initialText={comment.desc}
+          />
+        )}
         <div className="flex items-center gap-x-3 text-dark-light font-roboto text-sm mt-3 mb-3">
           {isUserLoggined && (
-            <button className="flex items-center space-x-2">
+            <button
+              className="flex items-center space-x-2"
+              onClick={() =>
+                setAffectedComment({ type: "replying", _id: comment._id })
+              }
+            >
               <FiMessageSquare className="w-4 h-auto" />
               <span>Balas</span>
             </button>
           )}
           {commentBelongsToUser && (
             <>
-              <button className="flex items-center space-x-2">
+              <button
+                className="flex items-center space-x-2"
+                onClick={() =>
+                  setAffectedComment({ type: "editing", _id: comment._id })
+                }
+              >
                 <FiEdit2 className="w-4 h-auto" />
                 <span>Edit</span>
               </button>
-              <button className="flex items-center space-x-2">
+              <button
+                className="flex items-center space-x-2"
+                onClick={() => deleteComment(comment._id)}
+              >
                 <FiTrash className="w-4 h-auto" />
                 <span>Hapus</span>
               </button>
             </>
           )}
         </div>
+        {isReplying && (
+          <CommentForm
+            btnLabel="Membalas"
+            formSubmitHandler={(value) =>
+              addComment(value, repliedCommentId, replyOnUserId)
+            }
+            formCancelHandler={() => setAffectedComment(null)}
+          />
+        )}
+        {replies.length > 0 && (
+          <div className="">
+            {replies.map((reply) => (
+              <Comment
+                key={reply._id}
+                addComment={addComment}
+                affectedComment={affectedComment}
+                setAffectedComment={setAffectedComment}
+                comment={reply}
+                deleteComment={deleteComment}
+                logginedUserId={logginedUserId}
+                replies={[]}
+                updateComment={updateComment}
+                parentId={comment._id}
+              />
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
--- src/components/comments/CommentsContainer.jsx
+++ src/components/comments/CommentsContainer.jsx
@@ -7,6 +7,7 @@ import Comment from "./Comment";
 const CommentsContainer = ({ className, logginedUserId }) => {
   const [comments, setComments] = useState([]);
   const mainComments = comments.filter((comment) => comment.parent === null);
+  const [affectedComment, setAffectedComment] = useState(null);
 
   console.log(comments);
 
@@ -19,7 +20,7 @@ const CommentsContainer = ({ className, logginedUserId }) => {
 
   const addCommentHandler = (value, parent = null, replyOnUser = null) => {
     const newComment = {
-      _id: "10",
+      _id: Math.random().toString(),
       user: {
         _id: "a",
         name: "Dedy",
@@ -28,22 +29,61 @@ const CommentsContainer = ({ className, logginedUserId }) => {
       post: "1",
       parent: parent,
       replyOnUser: replyOnUser,
-      createdAt: "2023-12-31T17:22:05.092+0000",
+      createdAt: new Date().toISOString(),
     };
     setComments((curState) => {
       return [newComment, ...curState];
     });
+    setAffectedComment(null);
+  };
+
+  const updateCommentHandler = (value, commentId) => {
+    const updatedComments = comments.map((comment) => {
+      if (comment._id === commentId) {
+        return { ...comment, desc: value };
+      }
+      return comment;
+    });
+    setComments(updatedComments);
+    setAffectedComment(null);
+  };
+
+  const deleteCommentHandler = (commentId) => {
+    const updatedComments = comments.filter((comment) => {
+      return comment._id !== commentId;
+    });
+    setComments(updatedComments);
+  };
+
+  const getRepliesHandler = (commentId) => {
+    return comments
+      .filter((comment) => comment.parent === commentId)
+      .sort((a, b) => {
+        return (
+          new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
+        );
+      });
   };
 
   return (
     <div className={`${className}`}>
       <CommentForm
-        btnLabel="Kirim"
+        btnLabel="Komentar"
         formSubmitHandler={(value) => addCommentHandler(value)}
       />
       <div className="space-y-4 mt-8">
         {mainComments.map((comment) => (
-          <Comment comment={comment} logginedUserId={logginedUserId} />
+          <Comment
+            key={comment._id}
+            comment={comment}
+            logginedUserId={logginedUserId}
+            affectedComment={affectedComment}
+            setAffectedComment={setAffectedComment}
+            addComment={addCommentHandler}
+            updateComment={updateCommentHandler}
+            deleteComment={deleteCommentHandler}
+            replies={getRepliesHandler(comment._id)}
+          />
         ))}
       </div>
     </div>
